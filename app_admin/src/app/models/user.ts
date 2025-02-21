ce2b0716ca3cab29950cd3bbb33bc92cfd25f6bd
sarah marshall <sarah.marshall5@snhu.edu>
2025-02-21 09:45:21 -0500

    Module 7 completed baseline

---
+++ app_admin/src/app/models/user.ts
@@ -0,0 +1,12 @@
+export class User { 
+    email: string; 
+    name: string; 
+
+    constructor() 
+    { 
+        this.email = ''; 
+        this.name = ''; 
+    } 
+}
+
+
--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -6,6 +6,7 @@ import { TripDataService } from '../services/trip-data.service';
 import { Trip } from '../models/trip';
 
 import { Router } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-trip-listing',
@@ -23,7 +24,8 @@ export class TripListingComponent implements OnInit {
 
   constructor(
     private tripDataService: TripDataService,
-    private router:Router
+    private router:Router,
+    private authenticationService: AuthenticationService
   ) {
     console.log('trip-listing constructor');
   }
@@ -49,6 +51,9 @@ export class TripListingComponent implements OnInit {
         console.log('Error: ' + error);
       }
     })
+  }
+  public isLoggedIn() { 
+    return this.authenticationService.isLoggedIn();
   }
 
   ngOnInit(): void {
